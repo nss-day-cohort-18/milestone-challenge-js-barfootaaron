ceef44415641ef93cc871fc972e1603bf74f6eb9
“Aaron <barfootaaron@yahoo.com>
2017-01-18 17:54:54 -0600

    finished sass day 1

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var treeHeight = document.getElementById("treeHeight");
+var characterInput = document.getElementById("characterInput");
+
+for (var characterInput=0; )
+
+
+document.getElementById("growYourTree")addEventListeners("click", function);
+document.getElementById("growYourTree")addEventListeners("onkeypress", function);
