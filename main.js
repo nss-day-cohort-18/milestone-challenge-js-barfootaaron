8ebc5ccdee3a48921753a98c180440e55ec04ba8
“Aaron <barfootaaron@yahoo.com>
2017-01-20 14:42:41 -0600

    finally can grow a tree

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 
-var treeHeight = document.getElementById("treeHeight");
-var characterInput = document.getElementById("characterInput");
+//CREATE VARIABLES
+var treeRoot = {size: 0, char: 0}
+var growButton = document.getElementById("growYourTree");
 
-for (var characterInput=0; )
+
+function tree(treeRoot) {
+	var treeHeight = parseInt(document.getElementById('treeHeight').value);
+	var charToUse = document.getElementById('characterInput').value;
+	treeRoot.size = treeHeight;
+	treeRoot.char = charToUse;
+	var numOfChars = 1;
+	var numOfSpaces = treeRoot.size -1;
+
+	if (treeRoot.size > 0 && treeRoot.char.length === 1) {
+		for (var i=0; i<treeRoot.size; i++) {
+			var spaces = ' '.repeat(numOfSpaces);
+			var character = treeRoot.char.repeat(numOfChars);
+			var treeDoIt = spaces + character;
+			console.log(treeDoIt);
+			numOfSpaces -= 1;
+			numOfChars += 2;
+		}
+	} 
+		//alert('Please be sure to enter a value in both fields!')
+
+	}
+
+//Grow tree when button pushed
+growButton.addEventListener('click', tree);
 
 
-document.getElementById("growYourTree")addEventListeners("click", function);
-document.getElementById("growYourTree")addEventListeners("onkeypress", function);
+
+tree(treeRoot);
